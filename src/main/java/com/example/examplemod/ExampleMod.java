90c2793b4acedfa36f029168eea1b7699dcea12f
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-13 21:22:07 +0200

    test commit

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -28,8 +28,7 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
-public class ExampleMod
-{
+public class ExampleMod {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger

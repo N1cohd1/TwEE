da17963d4a7f3332ae24976fa4b61c5d1233f54a
N1cohd1 <jakob.krutzlinger@web.de>
2023-05-14 20:22:45 +0200

    Balls

---
+++ src/main/java/com/N1cohd1/twee/items/ModItems.java
@@ -0,0 +1,21 @@
+package com.N1cohd1.twee.items;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.EventBus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.N1cohd1.twee.twee.MODID;
+
+public class ModItems {
+    public static final DeferredRegister<Item> MOD_ITEMS=DeferredRegister.create(ForgeRegistries.ITEMS,MODID);
+
+    public static final RegistryObject<Item> TRISTOS = MOD_ITEMS.register("tristos_flower", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
+    public static void register(IEventBus eventBus){
+        MOD_ITEMS.register(eventBus);
+    }
+}

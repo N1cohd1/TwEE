41daee0385245ed6674cc49e470e4fda90a1c3fe
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-16 15:04:04 +0200

    more datagenerators

--- src/main/java/com/N1cohd1/twee/datagen/DataGenerators.java
+++ src/main/java/com/N1cohd1/twee/datagen/DataGenerators.java
@@ -1,5 +1,8 @@
 package com.N1cohd1.twee.datagen;
 
+import com.N1cohd1.twee.datagen.models.ItemModels;
+import com.N1cohd1.twee.datagen.tags.TagsBlock;
+import com.N1cohd1.twee.datagen.tags.TagsItem;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,21 +15,20 @@ public class DataGenerators {
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
 
-        generator.addProvider(event.includeClient(), new BlockModels(generator, event.getExistingFileHelper()));
+        //generator.addProvider(event.includeClient(), new BlockModels(generator, event.getExistingFileHelper()));
         generator.addProvider(event.includeClient(), new ItemModels(generator, event.getExistingFileHelper()));
 
         TagsBlock blockTags = new TagsBlock(generator, event.getExistingFileHelper());
         generator.addProvider(event.includeServer(), blockTags);
         generator.addProvider(event.includeServer(), new TagsItem(generator, blockTags, event.getExistingFileHelper()));
-        generator.addProvider(event.includeServer(), new LootModifiers(generator));
-
-        // RECIPES
+        /* RECIPES
         generator.addProvider(event.includeServer(), new SmeltingRecipeProvider(generator));
         generator.addProvider(event.includeServer(), new BlastingRecipeProvider(generator));
         generator.addProvider(event.includeServer(), new StonecuttingRecipeProvider(generator));
+         */
 
         /* LOOT TABLES */
-        generator.addProvider(event.includeServer(), new LootTables(generator));
+        //generator.addProvider(event.includeServer(), new LootTables(generator));
 
     }
 }

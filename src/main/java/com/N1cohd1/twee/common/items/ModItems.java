96db115755f55a551dfefd2bc8fc67c4f650596c
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-28 21:32:04 +0200

    readd lost stuff

--- src/main/java/com/N1cohd1/twee/common/items/ModItems.java
+++ src/main/java/com/N1cohd1/twee/common/items/ModItems.java
@@ -1,6 +1,7 @@
 package com.N1cohd1.twee.common.items;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.FireChargeItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,6 +14,8 @@ public class ModItems {
     public static final DeferredRegister<Item> MOD_ITEMS=DeferredRegister.create(ForgeRegistries.ITEMS,MODID);
 
     public static final RegistryObject<Item> TRISTOS = MOD_ITEMS.register("tristos_flower", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
+    public static final RegistryObject<Item> TRISTOSV2 = MOD_ITEMS.register("tristosv2_flower", () -> new FireChargeItem(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         MOD_ITEMS.register(eventBus);
